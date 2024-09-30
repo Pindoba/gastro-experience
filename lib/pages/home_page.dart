f0c82e466a93b230a6ade00147c188ad45e71a81
Welton Moura <weltonmoura@live.com>
2024-09-30 11:33:47 -0300

    responsividade container de scroll horizontal

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,232 +16,236 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(0.0),
-          child: Stack(
-                children: [
-          SingleChildScrollView(
-            child: Column(
+        body: Stack(
               children: [
-                Stack(children: [
-                   SizedBox(width: MediaQuery.of(context).size.width,
-                   height: 400,
-                    child: Image.network(
-                        'https://www.bv.com.br/documents/d/bv-inspira/hobbies_que_dao_dinheiro-jpg',fit: BoxFit.cover,),
-                  ),
-                   Positioned(bottom: 10,
-                   left: -45,
-                    child: TextWidget(
-                      text:'''Casa do padim,
-                 Juazeiro do Norte, Ceará'''
-                   ,color: Cores.white,
-                   sizeText: 13,
-                   fontRoboto: true,
-                   bold: FontWeight.bold,
-                   ),)],
+        SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(children: [
+                 SizedBox(width: MediaQuery.of(context).size.width,
+                 height: 350,
+                  child: Image.network(
+                      'https://www.bv.com.br/documents/d/bv-inspira/hobbies_que_dao_dinheiro-jpg',fit: BoxFit.cover,),
                 ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                           TextWidget(
-                            text: 'Uninassau Indica',
-                            sizeText: 34,
-                            bold: FontWeight.bold, 
-                            color: Cores.primary),
+                 Positioned(bottom: 10,
+                 left: -45,
+                  child: TextWidget(
+                    text:'''Casa do padim,
+               Juazeiro do Norte, Ceará'''
+                 ,color: Cores.white,
+                 sizeText: 13,
+                 fontRoboto: true,
+                 bold: FontWeight.bold,
+                 ),)],
+              ),
 
-                          Row(
-                            children: [
-                              IconButton(
-                                  icon: const Icon(Icons.arrow_back_ios,size: 15),
-                                  onPressed: () {
-                                    _scrollController.animateTo(
-                                      _scrollController.offset - 280,
-                                      duration: const Duration(milliseconds: 500),
-                                      curve: Curves.easeInOut,
-                                    );
-                                  },
-                                ),
+                
 
-
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25,right: 25),
+                      child: Container(
+                        constraints: BoxConstraints(maxWidth: 1200),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                             TextWidget(
+                              text: 'Uninassau Indica',
+                              sizeText: 34,
+                              bold: FontWeight.bold, 
+                              color: Cores.primary),
+                                
+                            Row(
+                              children: [
                                 IconButton(
-                                  icon: Icon(Icons.arrow_forward_ios,size: 15),
-                                  onPressed: () {
-                                  _scrollController.animateTo(
-                                  _scrollController.offset + 280,
-                                  duration: Duration(milliseconds: 500),
-                                  curve: Curves.easeInOut,
-                                );
-                              },
+                                    icon: const Icon(Icons.arrow_back_ios,size: 15),
+                                    onPressed: () {
+                                      _scrollController.animateTo(
+                                        _scrollController.offset - 280,
+                                        duration: const Duration(milliseconds: 500),
+                                        curve: Curves.easeInOut,
+                                      );
+                                    },
+                                  ),
+                                
+                                  IconButton(
+                                    icon: Icon(Icons.arrow_forward_ios,size: 15),
+                                    onPressed: () {
+                                    _scrollController.animateTo(
+                                    _scrollController.offset + 280,
+                                    duration: Duration(milliseconds: 500),
+                                    curve: Curves.easeInOut,
+                                  );
+                                },
+                              ),
+                              ],
                             ),
-                            ],
-                          ),
-
-                        ],
+                                
+                          ],
+                        ),
                       ),
-                Container(
+                    ),
+              Padding(
+                padding: const EdgeInsets.only(left: 25,right: 25),
+                child: Container(
+                  constraints: BoxConstraints(maxWidth: 1200),
                   color: Colors.blue,
                   height: 300,
                   child: Row(
                            children: [
-                             SizedBox(
-                              width: MediaQuery.of(context).size.width,
-                               child: ListView.builder(
-                                controller: _scrollController,
-                                  itemCount: restaurantsRepository.length,
-                                  scrollDirection: Axis.horizontal,
-                                  shrinkWrap: true,
-                                  itemBuilder: (context, index) {
-                                    return 
-                                    SizedBox(
-                                      height: 180,
-                                      width: 280,
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: CardRestaurant(
-                                          restaurant: restaurantsRepository[index],
+                             Container(
+                              constraints: BoxConstraints(maxWidth: 1190,),
+                               child: SizedBox(
+                                width: MediaQuery.of(context).size.width -50,
+                                 child: ListView.builder(
+                                  controller: _scrollController,
+                                    itemCount: restaurantsRepository.length,
+                                    scrollDirection: Axis.horizontal,
+                                    shrinkWrap: true,
+                                    itemBuilder: (context, index) {
+                                      return 
+                                      SizedBox(
+                                        height: 180,
+                                        width: 280,
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: CardRestaurant(
+                                            restaurant: restaurantsRepository[index],
+                                          ),
                                         ),
-                                      ),
-                                    );
-                                  }),
+                                      );
+                                    }),
+                               ),
                              ),
-
+                        
                            ],
                          ),
+                                                 
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                color: const Color.fromARGB(255, 51, 243, 33),
+                width: double.infinity,
+                height: 250,
+                child: Column(
+                  children: [
+                    Text('Uninassau Indica'),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                color: Colors.blue,
+                width: double.infinity,
+                height: 250,
+                child: Column(
+                  children: [
+                    Text('Uninassau Indica'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
 
-                      
-          
-          
-                      // CardRestaurant(restaurant: restaurantsRepository[0],)
- 
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                Container(
-                  color: const Color.fromARGB(255, 51, 243, 33),
-                  width: double.infinity,
-                  height: 250,
-                  child: Column(
-                    children: [
-                      Text('Uninassau Indica'),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                Container(
-                  color: Colors.blue,
-                  width: double.infinity,
-                  height: 250,
-                  child: Column(
-                    children: [
-                      Text('Uninassau Indica'),
-                    ],
-                  ),
-                ),
+        Container(
+           decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.black.withOpacity(0.6),
+                Colors.transparent,
               ],
             ),
           ),
-          // Positioned(bottom: 30, child: Text('Casa do padim')),
-          // Container(color: Colors.blue,width: 100,height: 100,),
-          Container(
-
-
-             decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Colors.black.withOpacity(0.6),
-                  Colors.transparent,
-                ],
-              ),
-            ),
-
-
-
-            child: Padding(
-              padding: const EdgeInsets.only(top: 10.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SizedBox( height: 50,
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: CircleAvatar(
-                            child: Image.network(
-                                'https://png.pngtree.com/png-clipart/20230624/original/pngtree-cooking-logo-vector-png-image_9213639.png', fit: BoxFit.cover,),
-                          ),
+        
+        
+        
+          child: Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox( height: 50,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          child: Image.network(
+                              'https://png.pngtree.com/png-clipart/20230624/original/pngtree-cooking-logo-vector-png-image_9213639.png', fit: BoxFit.cover,),
                         ),
-                        TextWidget(text: 'Sabores Cariri',sizeText: 20,color: Cores.fontSubTitle,),
-                      ],
-                    ),
+                      ),
+                      TextWidget(text: 'Sabores Cariri',sizeText: 20,color: Cores.fontSubTitle,),
+                    ],
                   ),
-              
-              
-                  Container(
-                        height: 40,
-                        width: 400,
-                        decoration: BoxDecoration(
-                          color: Colors.white.withOpacity(0.5),
-                          borderRadius: BorderRadius.circular(8),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.2),
-                              spreadRadius: 2,
-                              blurRadius: 4,
-                              offset: Offset(0, 2),
-                            ),
-                          ],
-                        ),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Icon(Icons.search),
-                        ),
-                        SizedBox(
-                          height: 50,
-                          width: 350,
-                          child: TextFormField(
-                             controller: TextEditingController(),
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              focusedBorder: InputBorder.none,
-                              enabledBorder: InputBorder.none,
-                              errorBorder: InputBorder.none,
-                              disabledBorder: InputBorder.none,
-                              contentPadding: EdgeInsets.only(bottom: 8.0),
-                              hintText: 'Pesquise por tópicos ou palavras chaves',
-                              hintStyle: TextStyle(color: const Color.fromARGB(255, 78, 78, 78)),
-                            ),
-                            style: TextStyle(color: Colors.black),
+                ),
+            
+            
+                Container(
+                      height: 40,
+                      width: 400,
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            spreadRadius: 2,
+                            blurRadius: 4,
+                            offset: Offset(0, 2),
                           ),
-                        )
-                      ],
-                    ),
-              ),
-              
-              
-              
-              
-              
-                  Container(
-                    child: IconButton(onPressed: () {}, icon: Icon(Icons.menu,color: Colors.white,size: 30,),),
-                  )
-                ],
-              ),
+                        ],
+                      ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(Icons.search),
+                      ),
+                      SizedBox(
+                        height: 50,
+                        width: 350,
+                        child: TextFormField(
+                           controller: TextEditingController(),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            enabledBorder: InputBorder.none,
+                            errorBorder: InputBorder.none,
+                            disabledBorder: InputBorder.none,
+                            contentPadding: EdgeInsets.only(bottom: 8.0),
+                            hintText: 'Pesquise por tópicos ou palavras chaves',
+                            hintStyle: TextStyle(color: const Color.fromARGB(255, 78, 78, 78)),
+                          ),
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      )
+                    ],
+                  ),
             ),
-          )
-                ],
-              ),
-        ));
+            
+            
+            
+            
+            
+                Container(
+                  child: IconButton(onPressed: () {}, icon: Icon(Icons.menu,color: Colors.white,size: 30,),),
+                )
+              ],
+            ),
+          ),
+        )
+              ],
+            ));
   }
 }
