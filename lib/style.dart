7c9b6bae0d5b02c71f96ad998c0dfd2badf8ed67
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2024-11-06 15:07:37 -0300

    feat: correct somes colors to correct HEX

--- lib/style.dart
+++ lib/style.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-class Cores {
-  static Color primary = const Color.fromARGB(255, 141, 103, 72);
-  static Color card = Color.fromARGB(188, 141, 103, 72);
+class DefaultColors {
+  static Color primary = const Color(0xFF8D6748);
+  static Color card = const Color(0xFFA3856C);
   static Color segundary = const Color.fromARGB(80, 141, 103, 72);
-  static Color fontTitle = Color.fromARGB(251, 26, 24, 24);
-  static Color fontSubTitle = Color.fromARGB(160, 0, 0, 0);
+  static Color fontTitle = const Color.fromARGB(251, 26, 24, 24);
+  static Color fontSubTitle = const Color.fromARGB(160, 0, 0, 0);
   static Color white = const Color.fromARGB(255, 238, 238, 238);
-  static Color like = Color.fromARGB(255, 245, 165, 35);
+  static Color like = const Color.fromARGB(255, 245, 165, 35);
   static Color c3 = const Color.fromARGB(80, 141, 103, 72);
+  static Color mainBg = const Color(0xFFF7F7F7);
 }
 
 
