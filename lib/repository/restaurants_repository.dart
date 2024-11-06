724de3613ed795ae098a5c7dc5b4d4c6455a1703
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2024-11-06 15:15:15 -0300

    feat: add search restaurant method

--- lib/repository/restaurants_repository.dart
+++ lib/repository/restaurants_repository.dart
@@ -1,7 +1,32 @@
 import 'package:gastro_experience/models/restaurants.dart';
-// import 'package:gastro_experience/repository/address_repository.dart';
 import 'package:gastro_experience/api/api.dart';
 // import 'package:dio/dio.dart';
+
+class RestaurantFilter {
+  String? name;
+  int? cuisineId;
+  int? serviceId;
+  int? priceRange;
+
+  RestaurantFilter({
+    this.name,
+    this.cuisineId,
+    this.serviceId,
+    this.priceRange,
+  });
+
+  String get queryParams {
+    String params = "";
+
+    if (name != null) params += "name=$name";
+    if (cuisineId != null) params += "cuisineId=$cuisineId";
+    if (serviceId != null) params += "serviceId=$serviceId";
+    if (priceRange != null) params += "priceRange=$priceRange";
+
+    return params;
+  }
+}
+
 
 class RestaurantsRepository {
   Future getRestaurants() async {
@@ -19,167 +44,16 @@ class RestaurantsRepository {
       print(e);
     }
   }
+
+  Future<List<Restaurants>> searchRestaurants(RestaurantFilter filters) async {
+    try {
+      final response = await api.get<List<dynamic>>("/restaurants?${filters.queryParams}");
+
+      if (response.data == null) throw Exception("Cannot be null");
+      
+      return response.data!.map((e) => Restaurants.fromMap(e)).toList();
+    } catch(e){
+      throw Exception(e.toString());
+    }
+  }
 }
-
-// }
-// getEssabagaca() async {
-//   List<Restaurants> restaurantsRepository = await getRestaurants();
-// print(restaurantsRepository);
-//   return restaurantsRepository;
-// }
-
-// final List<Restaurants> restaurantsRepository = getEssabagaca();
-
-// final List<Restaurants> restaurantsRepository = [
-//   Restaurants(
-//       idRestaurant: 1,
-//       name: 'Vei do caldo',
-//       priceRange: 1,
-//       address: addressRepository[1],
-//       mainImage:
-//           'https://cdn.blablacar.com/wp-content/uploads/br/2024/05/05094735/comidas-sao-joao-2.webp',
-//       description:
-//           'Vei anda nas madrugadas de juazeiro vendendo o melhor caldo da região.',
-//       openingHours: '',
-//       phoneNumber: '88 9 8745 3652',
-//       whatsapp: '88 9 8745 3652',
-//       regional: true
-//       ),
-//   Restaurants(
-//       idRestaurant: 2,
-//       name: 'Ligerinho',
-//       priceRange: 4,
-//       address: addressRepository[0],
-//       mainImage:
-//           'https://cdn.prod.website-files.com/6549128db41d15ae06784cbc/65f80fca692a2d1ce106b45b_staff.webp',
-//       description:
-//           'A renomada chef Bel Coelho é conhecida pelo seu compromisso com uma alimentação saudável, uso de ingredientes autóctones e preservação das diversas culturas alimentares brasileiras. Neste espaço acolhedor e casual, partilhado com a Livraria Megafauna no icônico edifício Copan, pode-se desfrutar a qualquer momento do dia, seja entre as prateleiras da livraria, em frente à cozinha ou na área externa ao ar livre. No menu, há opções que vão desde tostadas e pão de queijo até Baião de dois. Alguns dos pratos imperdíveis incluem o apetitoso Tempurá de lula na tapioca e ponzu de caju, o suculento Nosso Oswaldo aranha. E, para finalizar, o Toucinho do céu com castanha do Pará, acompanhado por um sedoso creme de puxuri e laranja. Há uma ótima seleção de vinhos orgânicos e drinks de autor!',
-//       openingHours: '',
-//       phoneNumber: '88 9 8745 3652',
-//       whatsapp: '88 9 8745 3652',
-//       isFavorite: true,
-//       stamp: true
-
-//       ),
-//   Restaurants(
-//       idRestaurant: 3,
-//       name: 'Ligerinho',
-//       priceRange: 3,
-//       address: addressRepository[0],
-//       mainImage:
-//           'https://aws-br-pic.kwai.net/kimg/EKzM1y8qmQEKAnMzEg1waG90by1vdmVyc2VhGoMBdXBpYy8yMDIyLzA3LzEyLzIzL0JNakF5TWpBM01USXlNek0wTWpsZk1UVXdNREF4TURreE9UZzFOVFF4WHpFMU1ERXdNREk0TkRReE9Ea3dNRjh4WHpNPV9vdXVfQmU4MDgyNTBhNjNmYjkwN2Y2OTYxM2UyNDE2ZjQ5ZDA0LndlYnA.webp',
-//       description:
-//           'O sanduiche mais higienico da cidade',
-//       openingHours: '',
-//       phoneNumber: '88 9 8745 3652',
-//       whatsapp: '88 9 8745 3652'),
-//   Restaurants(
-//       idRestaurant: 4,
-//       name: 'Ligerinho',
-//       priceRange: 2,
-//       address: addressRepository[0],
-//       mainImage:
-//           'https://images02.brasildefato.com.br/ed98b805f8ec271f437e2a462dd4719a.webp',
-//       description:
-//           'O sanduiche mais higienico da cidade',
-//       openingHours: '',
-//       phoneNumber: '88 9 8745 3652',
-//       whatsapp: '88 9 8745 3652'),
-//   Restaurants(
-//       idRestaurant: 5,
-//       name: 'Ligerinho',
-//       priceRange: 1,
-//       address: addressRepository[0],
-//       mainImage:
-//           'https://institucional.ifood.com.br/wp-content/uploads/2024/07/comidas-tipicas-brasileiras-virado-paulista.jpg',
-//       description:
-//           'O sanduiche mais higienico da cidade',
-//       openingHours: '',
-//       phoneNumber: '88 9 8745 3652',
-//       whatsapp: '88 9 8745 3652'),
-//   Restaurants(
-//       idRestaurant: 1,
-//       name: 'Ligerinho',
-//       priceRange: 1,
-//       address: addressRepository[0],
-//       mainImage:
-//           'https://mir-s3-cdn-cf.behance.net/project_modules/fs/ec4d9c98203055.5ed6dbcb95425.png',
-//       description:
-//           'O sanduiche mais higienico da cidade',
-//       openingHours: '',
-//       phoneNumber: '88 9 8745 3652',
-//       whatsapp: '88 9 8745 3652'),
-//   Restaurants(
-//       idRestaurant: 1,
-//       name: 'Ligerinho',
-//       priceRange: 1,
-//       address: addressRepository[0],
-//       mainImage:
-//           'https://mir-s3-cdn-cf.behance.net/project_modules/fs/ec4d9c98203055.5ed6dbcb95425.png',
-//       description:
-//           'O sanduiche mais higienico da cidade',
-//       openingHours: '',
-//       phoneNumber: '88 9 8745 3652',
-//       whatsapp: '88 9 8745 3652'),
-//   Restaurants(
-//       idRestaurant: 1,
-//       name: 'Ligerinho',
-//       priceRange: 1,
-//       address: addressRepository[0],
-//       mainImage:
-//           'https://mir-s3-cdn-cf.behance.net/project_modules/fs/ec4d9c98203055.5ed6dbcb95425.png',
-//       description:
-//           'O sanduiche mais higienico da cidade',
-//       openingHours: '',
-//       phoneNumber: '88 9 8745 3652',
-//       whatsapp: '88 9 8745 3652'),
-//   Restaurants(
-//       idRestaurant: 1,
-//       name: 'Ligerinho',
-//       priceRange: 1,
-//       address: addressRepository[0],
-//       mainImage:
-//           'https://mir-s3-cdn-cf.behance.net/project_modules/fs/ec4d9c98203055.5ed6dbcb95425.png',
-//       description:
-//           'O sanduiche mais higienico da cidade',
-//       openingHours: '',
-//       phoneNumber: '88 9 8745 3652',
-//       whatsapp: '88 9 8745 3652'),
-//   Restaurants(
-//       idRestaurant: 1,
-//       name: 'Ligerinho',
-//       priceRange: 1,
-//       address: addressRepository[0],
-//       mainImage:
-//           'https://mir-s3-cdn-cf.behance.net/project_modules/fs/ec4d9c98203055.5ed6dbcb95425.png',
-//       description:
-//           'O sanduiche mais higienico da cidade',
-//       openingHours: '',
-//       phoneNumber: '88 9 8745 3652',
-//       whatsapp: '88 9 8745 3652'),
-//   Restaurants(
-//       idRestaurant: 1,
-//       name: 'Ligerinho',
-//       priceRange: 1,
-//       address: addressRepository[0],
-//       mainImage:
-//           'https://mir-s3-cdn-cf.behance.net/project_modules/fs/ec4d9c98203055.5ed6dbcb95425.png',
-//       description:
-//           'O sanduiche mais higienico da cidade',
-//       openingHours: '',
-//       phoneNumber: '88 9 8745 3652',
-//       whatsapp: '88 9 8745 3652'),
-//   Restaurants(
-//       idRestaurant: 1,
-//       name: 'Ligerinho',
-//       priceRange: 1,
-//       address: addressRepository[0],
-//       mainImage:
-//           'https://mir-s3-cdn-cf.behance.net/project_modules/fs/ec4d9c98203055.5ed6dbcb95425.png',
-//       description:
-//           'O sanduiche mais higienico da cidade',
-//       openingHours: '',
-//       phoneNumber: '88 9 8745 3652',
-//       whatsapp: '88 9 8745 3652'),
-
-// ];
