d1008f43eacb0d34cd19aec7cc85c51add251afc
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2024-11-06 15:07:48 -0300

    feat: add rating icons to assets

--- lib/assets.dart
+++ lib/assets.dart
@@ -1,3 +1,5 @@
- class Assets {
- static String capa = 'assets/img/capa.jpeg';
+class Assets {
+  static String capa = 'assets/img/capa.jpeg';
+  static String ratingIcon = 'assets/rating_icon.webp';
+  static String ratingIconFilled = 'assets/rating_icon_filled.webp';
 }
