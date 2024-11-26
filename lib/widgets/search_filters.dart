a4a23d7e048f0c1459a633868a30a0484850633d
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2024-11-26 07:50:44 -0300

    feat: create initial search filter

---
+++ lib/widgets/search_filters.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:gastro_experience/controller/search_filter_controller.dart';
+import 'package:gastro_experience/models/cuisine.dart';
+import 'package:gastro_experience/models/restaurant_filter.dart';
+import 'package:gastro_experience/models/stamp.dart';
+import 'package:gastro_experience/style.dart';
+import 'package:gastro_experience/widgets/text.dart';
+
+class SearchFilters extends StatefulWidget {
+  final Function(RestaurantFilter filters) onSearch;
+  const SearchFilters({super.key, required this.onSearch});
+
+  @override
+  State<SearchFilters> createState() => _SearchFiltersState();
+}
+
+class _SearchFiltersState extends State<SearchFilters> {
+  late SearchFilterController controller;
+
+  @override
+  void initState(){
+    super.initState();
+    controller = SearchFilterController(onSearch: widget.onSearch);
+    controller.init();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListenableBuilder(
+      listenable: controller,
+      builder: (context, _) {
+        return Row(
+          children: [
+            IconButton.filled(
+              icon: const Icon(Icons.filter_alt),
+              onPressed: () => {},
+            ),
+            const SizedBox(width: 8),
+            ElevatedButton(
+              child: DefaultText.normal("Limpar filtros"),
+              onPressed: () => {},
+            ),
+            ...List.generate(
+              controller.stamps.length * 2,
+              (index){
+                bool isSeparator = index % 2 == 0;
+
+                if (isSeparator) return const SizedBox(width: 8);
+
+                int realIndex = index ~/ 2;
+                Stamp stamp = controller.stamps[realIndex];
+                bool isSelected = controller.stamps[realIndex] == controller.selectedStamp;
+
+                return ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: isSelected ? WidgetStatePropertyAll(DefaultColors.primary) : null
+                    ),
+                    onPressed: () => controller.toggleStamp(stamp),
+                    child: DefaultText.normal(stamp.name, isInverted: isSelected,)
+                );
+            }),
+            const SizedBox(width: 8),
+            Flexible(
+              child: LayoutBuilder(
+                builder: (_, constraints) {
+                  final width = constraints.maxWidth * 0.5;
+                  return DropdownMenu<Cuisine>(
+                    hintText: "Tipo de Cozinha",
+                    onSelected: controller.setCuisine,
+                    width: width > 200 ? 200 : width,
+                    dropdownMenuEntries: controller.cuisines.map<DropdownMenuEntry<Cuisine>>((Cuisine cuisine) {
+                      return DropdownMenuEntry<Cuisine>(value: cuisine, label: cuisine.label);
+                    }).toList(),
+                  );
+                }
+              ),
+            ),
+          ]
+        );
+      }
+    );
+  }
+}
